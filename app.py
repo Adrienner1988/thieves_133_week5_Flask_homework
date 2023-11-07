3ad50f28c44943a775e4b5cdbf2b3d621bdba7a4
Adrienne Daniels <106035434+Adrienner1988@users.noreply.github.com>
2023-11-07 17:04:11 -0500

    corrected in jinja in search.html file, corrected tthe helper code.

--- app.py
+++ app.py
@@ -14,23 +14,21 @@ def hello_trainers():
 @app.route('/search', methods=['GET', 'POST'])
 def pokemon_search():
     if request.method == 'POST':
-        data = requests.form.get('data')
-
-        url = f'https://pokeapi.co/api/v2/pokemon/{data}'
-        response = requests.get(url)
+        data = request.form.get('data').lower()
+        print(data)
         try:
-            data = response.json()
-            return pokemon_search('data')
-        except IndexError:
-            return 'Invalid Pokéman'
+            url = f'https://pokeapi.co/api/v2/pokemon/{data}'
+            response = requests.get(url)
+            more_data = response.json()
+            get_poke = get_pokemon_data(more_data)
+            return render_template('search.html', data=get_poke)
+        except: 
+            return render_template('search.html')
     else:
          return render_template('search.html')
-
         
 def get_pokemon_data(data):
-        new_pokeman_data = []
-        for pokeman in data:
-            pokemon_dict = {
+        pokemon_dict = {
                 'name': data['forms'][0]['name'],
                 'ability': data['abilities'][0]['ability']['name'],
                 'base_experience': data['base_experience'],
@@ -39,9 +37,19 @@ def get_pokemon_data(data):
                 'hp_stat': data['stats'][0]['base_stat'],
                 'defense_stat': data['stats'][2]['base_stat']
             }
-            new_pokeman_data.append(pokemon_dict)
-        return new_pokeman_data
+        return pokemon_dict
 
-# @app.route('/pokeInformation', methods=['GET', 'POST'])
-# 
+     
+        
+
+
+
+
     
+    
+      
+
+        
+# def get_pokemon_data(data):
+        # new_pokemon_data = []
+        # for pokemon in data:
